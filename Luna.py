d9328e9195fe050d3ec4798b60c7613ead453e59
Lauchlon <80520507+LauchlonSmith@users.noreply.github.com>
2021-04-03 18:09:16 +1000

    Add files via upload

---
+++ Luna.py
@@ -0,0 +1,19 @@
+import os.path
+import glob
+import time
+import os
+
+if glob.glob('./*.l'):
+	print("Welcome to the luna enviroment\nMade By: Lauchlon")
+	time.sleep(5)
+	os.startfile('main.l')
+
+
+
+
+
+
+
+# Leave at end of code
+else:
+    print ("Lang unable to start")
